48451f2cc3909f83a58ebba27142bbaecc57c44c
Hanna Likhtman <hanna.likhtman@hrforecast.de>
2020-05-16 15:19:54 +0800

    pro-production configuration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,10 +1,11 @@
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, ExtractJwt } from 'passport-jwt';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtPayload } from './jwt-payload.interface';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UserRepository } from './user.repository';
-import { User } from './user.entity';
+import { PassportStrategy } from '@nestjs/passport'
+import { Strategy, ExtractJwt } from 'passport-jwt'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { JwtPayload } from './jwt-payload.interface'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserRepository } from './user.repository'
+import { User } from './user.entity'
+import * as config from 'config'
 
 @Injectable()
 export class JwtStrategy extends  PassportStrategy(Strategy) {
@@ -14,13 +15,13 @@ export class JwtStrategy extends  PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'topSecret51',
-    });
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
+    })
   }
 
   async validate(payload: JwtPayload): Promise<User> {
     const { username } = payload;
-    const user = await this.userRepository.findOne({ username });
+    const user = await this.userRepository.findOne({ username })
 
     if (!user) {
       throw new UnauthorizedException('Invalid credentials')
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as config from 'config'
 
 async function bootstrap() {
   const logger = new Logger('Logger')
-
   const app = await NestFactory.create(AppModule)
-  const port = 3000
-  await app.listen(port)
 
+  const serverConfig = config.get('server')
+
+  const port = process.env.PORT || serverConfig.port
+  await app.listen(port)
   logger.debug(`Application listening on port ${port}`)
 }
 
